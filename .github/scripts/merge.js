d9d7b19b14140434546dedde441384fae61ad270
Krish Gupta <m.krishggupta@gmail.com>
2024-11-03 06:29:50 +0530

    is extension needed for require?

--- .github/scripts/merge.js
+++ .github/scripts/merge.js
@@ -2,9 +2,19 @@
 /**
  * @param {{ lgtmPRs: {repo: string;number: number;url: string;ref: string;}[], failedPRs: {repo: string;number: number;url: string;ref: string;}[] }} prs
  * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
-module.exports = async ({ lgtmPRs, failedPRs }, { core, exec }) => {
+module.exports = async ({ lgtmPRs, failedPRs }, { core, github, exec }) => {
   for (const pr of lgtmPRs) {
     core.startGroup(`${pr.repo}#${pr.number}`);
+
+    const archived = await github.rest.repos
+      .get({ owner: pr.repo.split("/")[0], repo: pr.repo.split("/")[1] })
+      .then(({ data }) => data.archived);
+
+    if (archived) {
+      core.warning("Archived repository, skipping");
+      core.endGroup();
+      continue;
+    }
 
     await exec.exec("gh", [
       "pr",
